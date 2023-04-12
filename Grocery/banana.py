954b323ad21164540f5bc8e185da3b82412968ab
yangchang-n <tomtomato@naver.com>
2023-04-12 10:42:51 +0900

    modified

--- Grocery/banana.py
+++ Grocery/banana.py
@@ -19,18 +19,19 @@ class Character(pygame.sprite.Sprite) :
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+        self.init_speed = 5
 
     def update(self, keys) :
 
         if keys[pygame.K_w] :
-            self.rect.y -= 3
+            self.rect.y -= self.init_speed
         elif keys[pygame.K_s] :
-            self.rect.y += 3
+            self.rect.y += self.init_speed
 
         if keys[pygame.K_a] :
-            self.rect.x -= 3
+            self.rect.x -= self.init_speed
         elif keys[pygame.K_d] :
-            self.rect.x += 3
+            self.rect.x += self.init_speed
 
 class Oobleck(pygame.sprite.Sprite) :
 
@@ -54,16 +55,41 @@ class Oobleck(pygame.sprite.Sprite) :
         elif self.rect.right > screen_width :
             self.x_speed = - self.x_speed
 
+class Konjac(pygame.sprite.Sprite) :
+
+    def __init__(self, x, y) :
+        super().__init__()
+        self.image = pygame.Surface((30, 30))
+        self.image.fill((120, 120, 120))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+        self.x_speed = random.randrange(1, 3 + 1)
+        self.y_speed = random.randrange(1, 3 + 1)
+
+    def update(self) :
+        self.rect.move_ip(self.x_speed, self.y_speed)
+        if self.rect.top < 0 :
+            self.y_speed = - self.y_speed
+        elif self.rect.bottom > screen_height :
+            self.y_speed = - self.y_speed            
+        elif self.rect.left < 0 :
+            self.x_speed = - self.x_speed            
+        elif self.rect.right > screen_width :
+            self.x_speed = - self.x_speed
+
 all_sprites = pygame.sprite.Group()
 
 character_xpos, character_ypos = screen_width / 2, screen_height / 2
 oobleck_xpos, oobleck_ypos = random.randrange(20, screen_width - 20 + 1), random.randrange(20, screen_height - 20 + 1)
+konjac_xpos, konjac_ypos = random.randrange(20, screen_width - 20 + 1), random.randrange(20, screen_height - 20 + 1)
 
 character = Character(character_xpos, character_ypos)
 oobleck = Oobleck(oobleck_xpos, oobleck_ypos)
+konjac = Konjac(konjac_xpos, konjac_ypos)
 
 all_sprites.add(character)
 all_sprites.add(oobleck)
+all_sprites.add(konjac)
 
 running = True
 
@@ -81,6 +107,8 @@ while running :
 
     character.update(keys)
     oobleck.update()
+    konjac.update()
+
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
     pygame.display.update()
